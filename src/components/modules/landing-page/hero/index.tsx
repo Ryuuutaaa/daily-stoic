039ea11e4bfdb1651675e258fecdfce4ba99e207
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-06-28 18:32:17 +0800

    feat : add background black and opacity

--- src/components/modules/landing-page/hero/index.tsx
+++ src/components/modules/landing-page/hero/index.tsx
@@ -3,11 +3,11 @@ import React, { JSX } from "react";
 const Hero = (): JSX.Element => {
   return (
     <div className="bg-[url('/images/bgseneca.jpg')] bg-cover bg-no-repeat bg-top bg-fixed min-h-screen w-full">
-      <section className="max-w-[120px] mx-auto flex items-center min-h-screen px-4">
-        <div className="flex flex-col select-none text-white">
-          <h6 className="text-sm md:text-lg lg:text-6xl font-black tracking-tight pb-2"></h6>
-        </div>
-      </section>
+      <div className="bg-black opacity-30">
+        <section className="max-w-[120px] mx-auto flex items-center min-h-screen px-4">
+          <div className="flex flex-col select-none text-white"></div>
+        </section>
+      </div>
     </div>
   );
 };
